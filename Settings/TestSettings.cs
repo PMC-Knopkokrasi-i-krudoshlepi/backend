911c3d560a2b1346316d7a0a69844af753dff1ce
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2023-04-16 21:11:47 +0500

    add test service

---
+++ Settings/TestSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace DPOBackend.Settings;
+
+public class TestSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string BooksCollectionName { get; set; } = null!;
+}
