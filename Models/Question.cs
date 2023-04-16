ffc4141982858d16806a08b2d1a0d2007da89c2a
m1nus0ne <Voldemar7455@Gmail.com>
2023-04-16 21:12:23 +0500

    add test model

---
+++ Models/Question.cs
@@ -0,0 +1,30 @@
+﻿namespace DPOBackend.Models;
+
+public abstract class Question
+{
+    public int id;
+    public QuestionType type;
+}
+
+public class SingleQuestion : Question
+{
+    public QuestionType type = QuestionType.SINGLE;
+    public List<string> possibleAnswers;
+    public string anser;
+    public string rightAnswer;
+}
+
+public class ManyQuestions : Question
+{
+    public QuestionType type = QuestionType.MANY;
+    public List<string> possibleAnswers;
+    public List<string> answer;
+    public List<string> rightAnswer;
+}
+
+public class FreeQuestions : Question
+{
+    public QuestionType type = QuestionType.FREE;
+    public string answer;
+    public string rightAnswer;
+}
--- Models/TestModel.cs
+++ Models/TestModel.cs
@@ -1,6 +1,22 @@
 ﻿namespace DPOBackend.Models;
 
-public class TestModel
+public class CourseTestModel
 {
-    
+    public int id;
+    public string name;
+    public string description;
+    public List<int> linkedCoursesIds;
+    public List<Question> questionsList;
+
 }
+
+public class PersonalityTestModel
+{
+    public int id;
+    public string name;
+    public string description;
+    
+    public List<Question> questionsList;
+
+}
+
