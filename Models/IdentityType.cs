f5e297384153e42493278988434960812355be96
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2023-04-23 14:37:43 +0500

    add IdentityType

---
+++ Models/IdentityType.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DPOBackend.Models;
+
+public class IdentityType
+{
+    [BsonElement("Name")]
+    public string Name{ get; set; }
+    
+    [BsonElement("Description")]
+    public string Description{ get; set; }
+}
