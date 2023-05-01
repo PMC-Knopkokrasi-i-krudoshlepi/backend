9c092a662ae224ac9e6520184e45618ced625c80
ShpakovDaniel <44341164+Reltig@users.noreply.github.com>
2023-05-01 18:45:31 +0500

    add profession model

---
+++ Models/Profession.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+public class Profession
+{
+    [BsonElement("Name")]
+    public string Name{ get; set; }
+    
+    [BsonElement("Description")]
+    public string Description{ get; set; }
+    
+    [BsonElement("SallaryFrom")]
+    public int SallaryFrom{ get; set; }
+    
+    [BsonElement("SallaryTo")]
+    public int SallaryTo{ get; set; }
+    
+    [BsonElement("Skills")]
+    public string[] Skills{ get; set; }
+    
+    [BsonElement("Tasks")]
+    public string Tasks{ get; set; }
+}
